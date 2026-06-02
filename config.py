4154aa112029fd5d6aac803698c1be0e9f42c0d9
Matthew Eastwood <ilovelego@outlook.com>
2026-06-02 13:20:03 -0400

    create function to initiate connection to database

--- config.py
+++ config.py
@@ -1,1 +1,6 @@
 serpApiKey = "https://serpapi.com/search?engine=google_jobs"
+params = {
+        'q':'Junior Software Developer',
+        'location':'Waterdown',
+        'api_key':'4283f57cada3ed115775418dff33d2550933323a2809c4429f07eaa2a7fb2ec7'
+    }
---
+++ data.py
@@ -0,0 +1,45 @@
+import sqlite3
+
+def InitiateConnection():
+    connection = sqlite3.connect('Postings.db')
+    cursor = connection.cursor()
+
+    if cursor.execute("select name from sqlite_master where name='postings'").fetchone() is None:
+        cursor.execute(
+            "create table postings (" \
+                "id integer primary key," \
+                "title text," \
+                "company text," \
+                "source text," \
+                "fetched_at datetime," \
+                "analyzed boolean default 0" \
+            ");"
+        )
+    if cursor.execute("select name from sqlite_master where name='insights'").fetchone() is None:
+        cursor.execute(
+            "create table postings (" \
+                "posting_id integer references postings(id)," \
+                "keywords text," \
+                "required text," \
+                "nice_to_have text," \
+                "seniority text" \
+                "analyzed_at datetime" \
+            ");"
+        )
+
+    return cursor
+
+def WriteToDB(postings):
+    cursor = InitiateConnection()
+    for posting in postings:
+        cursor.execute("")
+
+
+def ReadFromDB():
+    return asdf
+
+
+
+"""
+https://docs.python.org/3/library/sqlite3.html
+"""
